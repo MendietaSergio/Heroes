71219961e705255383b0f285fa0d3871c13029ac
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-09 18:12:28 -0300

    Añade login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+import { types } from "../types";
 
 export const Login = () => {
-    return (
-        <div>
-            <h1>Logín</h1>
-            <hr/>
-        </div>
-    )
-}
+  const { dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  //si encuentra su ultima ruta, lo redireccion a la misma. sino lo envia al home
+  const lastPath = localStorage.getItem('lastPath') || '/'
+
+  const handleLogin = () => {
+    dispatch({
+      type: types.login,
+      payload: {
+          name: "Sergio"
+        }
+    });
+    navigate(lastPath)
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Logín</h1>
+      <hr />
+      <button onClick={handleLogin} className="btn btn-primary">
+        Logín
+      </button>
+    </div>
+  );
+};
