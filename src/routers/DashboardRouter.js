ff4d02527428fe4c99e4c82d1c753affcdc3cacc
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-09 18:10:30 -0300

    Añade dashboardrouter

--- src/routers/DashboardRouter.js
+++ src/routers/DashboardRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { Dc } from "../pages/Dc.js";
 import { Marvel } from "../pages/Marvel.js";
 import { Search } from "../pages/Search.js";
@@ -7,6 +7,8 @@ import { Heroe } from "../pages/Heroe.js";
 import { Navbar } from "../components/Navbar.js";
 
 export const DashboardRouter = () => {
+  const {pathname} = useLocation()
+  localStorage.setItem('lastPath', pathname)
   return (
     <>
     <Navbar/>
@@ -14,7 +16,7 @@ export const DashboardRouter = () => {
         <Route path="/marvel" element={<Marvel />} />
         <Route path="/dc" element={<Dc />} />
         <Route path="/search" element={<Search />} />
-        <Route path="/heroe/:id" element={<Heroe />} />
+        <Route path="/heroe/:heroeID" element={<Heroe />} />
         <Route
           path="/*"
           element={
