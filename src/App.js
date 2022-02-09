bd7edd03cca74364722b7ce99d7462d24b35588e
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-09 18:19:24 -0300

    Añade app con sus componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import {React} from 'react'
-import { AppRouter } from './routers/AppRouter';
+import { React, useReducer, useEffect } from "react";
+import { AuthContext } from "./auth/AuthContext";
+import { AuthReducer } from "./auth/AuthReducer";
+import { AppRouter } from "./routers/AppRouter";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || {
+    logueado : false
+  }
+};
+
 function App() {
+  const [user, dispatch] = useReducer(AuthReducer, {}, init);
+
+  useEffect(() => {
+    //cuando user cambie, se cambia el estado user en localstorage
+    localStorage.setItem("user", JSON.stringify(user, null, 3));
+  }, [user]);
   return (
-    <div className="container">
-      <AppRouter/>
-    </div>
+    <AuthContext.Provider
+      value={{
+        user,
+        dispatch,
+      }}
+    >
+      <div className="container">
+        <AppRouter />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
