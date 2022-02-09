82acb74bd6a81b063a016b0c96e062b2a264eb89
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-09 18:11:43 -0300

    Añade appRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Login } from '../pages/Login'
-import { DashboardRouter } from './DashboardRouter'
+import React, { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+import { Login } from "../pages/Login";
+import { DashboardRouter } from "./DashboardRouter";
 export const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <div className='mt-3'>
-                <Routes>
-                    <Route path='/login' element={<Login/>} />
-                    <Route path='/*' element={ <DashboardRouter/> } />
-                </Routes>
-            </div>
-        </BrowserRouter>
-    )
-}
+  const { user } = useContext(AuthContext);
+
+  const estaLogueado = user.logueado
+
+  return (
+    <BrowserRouter>
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/login" element={!estaLogueado ?  <Login /> : <Navigate toy='/'/>} />
+          <Route path="/*" element={estaLogueado ? <DashboardRouter />:<Navigate to='/login' />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
