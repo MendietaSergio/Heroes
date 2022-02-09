3b162b0ba5ea9c954637192323e4da80660d8c38
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-09 18:19:03 -0300

    Añade navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,21 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+import { types } from "../types";
 export const Navbar = () => {
+  
+const navigate = useNavigate()
+
+  const {user, dispatch}=  useContext(AuthContext)
+  const { name} = user;
+
+  const handleLogout = () =>{
+    dispatch({
+      type : types.logout,
+      payload : null
+    })
+    navigate('/login')
+  }
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -33,6 +48,20 @@ export const Navbar = () => {
             </div>
           </div>
         </div>
+        <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul className="navbar-nav ms-auto">
+                    <p className="nav-item nav-link text-info mb-0">
+                        {name}
+                    </p>
+                    <button
+                        className="nav-item nav-link border-0 bg-transparent"
+                        onClick={handleLogout}
+                    >
+                        Logout
+                    </button>
+                </ul>
+            </div>
+
       </nav>
     </div>
   );
