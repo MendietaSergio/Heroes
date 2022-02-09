e64e145675f47a379cb5d0f2474646688e576872
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-09 18:12:04 -0300

    Añade el buscador

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React from "react";
+import {useSearchParams} from 'react-router-dom'
+import {useForm} from '../hooks/useForm'
+import { getHeroesBySearch } from "../selectors/getHeroesBySearch";
 
 export const Search = () => {
-    return (
-        <div>
-            <h1>Buscador</h1>
-            <hr/>
-        </div>
-    )
-}
+  
+  const [searchParams, setSearchParams] = useSearchParams({
+      keyword : ""
+  })
+  
+  const [{keyword}, handleInputChange] = useForm({
+      keyword : searchParams.get('keyword')
+  })
+  const resultado = getHeroesBySearch(searchParams.get('keyword'))
+  console.log(resultado);
+    const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchParams({
+        keyword
+    })
+  };
+
+  return (
+    <div className="row">
+      <div className="col-3">
+        <h4>Busqueda</h4>
+        <hr />
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Buscá tu héroe"
+            className="form-control"
+            name="keyword"
+            value={keyword}
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
+
+      <div className="col93"></div>
+    </div>
+  );
+};
