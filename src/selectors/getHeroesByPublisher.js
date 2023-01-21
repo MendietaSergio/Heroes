9cb9078b6e724d5fa7f75e9fbe5c142c61ad3bb6
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2023-01-21 19:07:38 -0300

    UPDATE: publisher === 'Todos'

--- src/selectors/getHeroesByPublisher.js
+++ src/selectors/getHeroesByPublisher.js
@@ -2,9 +2,8 @@ import { heroes } from "../data/heroes";
 
 export const getHeroesByPublisher = (publisher) => {
   const validPublisher = ["DC Comics", "Marvel Comics"];
-  if (!validPublisher.includes(publisher)) {
-    //mensaje de error
-    throw new Error(`Publisher ${publisher} no válido. ${validPublisher}`);
+  if (publisher === "Todos") {
+    return heroes;
   } else {
     return heroes.filter((heroe) => heroe.publisher === publisher);
   }
