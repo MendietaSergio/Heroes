4a6329a8199eb2ec4af2b31af99f8a7d669e9c34
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2023-01-21 19:06:10 -0300

    UPDATE: navigate , view found page

--- src/routers/DashboardRouter.js
+++ src/routers/DashboardRouter.js
@@ -1,26 +1,25 @@
 import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import { Dc } from "../pages/Dc.js";
-import { Marvel } from "../pages/Marvel.js";
+import { Routes, Route } from "react-router-dom";
+import { Filter } from "../pages/Filter.js";
 import { Search } from "../pages/Search.js";
 import { Heroe } from "../pages/Heroe.js";
 import { Navbar } from "../components/Navbar.js";
 
 export const DashboardRouter = () => {
-  const {pathname} = useLocation()
-  localStorage.setItem('lastPath', pathname)
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/marvel" element={<Marvel />} />
-        <Route path="/dc" element={<Dc />} />
+        <Route path="/marvel" element={<Filter params={"Marvel Comics"} />} />
+        <Route path="/dc" element={<Filter params={"DC Comics"} />} />
+        <Route path="/todos" element={<Filter params={"Todos"} />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/heroe/:heroeID" element={<Heroe />} />
         <Route path="/heroe/:heroeID" element={<Heroe />} />
         <Route
           path="/*"
           element={
-            <main style={{ padding: "1rem" }}>
+            <main className="found">
               <p>Nada por aquí!</p>
             </main>
           }
